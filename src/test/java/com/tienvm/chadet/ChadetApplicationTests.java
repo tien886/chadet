84d91baa4cc4bdafa929094c189fb80a19fed092
tientrysoft <tienvm886@gmail.com>
2026-08-07 09:06:44 +0700

    feat: init codebase

---
+++ src/test/java/com/tienvm/chadet/ChadetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienvm.chadet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChadetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
